7a32d57be69a594d46a40232cdadccc60a823190
XxJon95 <jonas.riedinger@t-online.de>
2026-02-20 18:25:08 +0100

    Bump cache version to v9

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,23 @@
-const CACHE_NAME = "halbmarathon-cache-v8";
+const CACHE_NAME = "halbmarathon-cache-v9";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("fetch", event => {
-  if (event.request.url.includes("docs.google.com")) {
-    // Nicht cachen – immer frisch laden
-    event.respondWith(fetch(event.request));
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
-  }
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
