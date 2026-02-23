9589891a790c022d4af4676597a43698f76b9390
XxJon95 <jonas.riedinger@t-online.de>
2026-02-23 11:32:13 +0100

    Bump cache version to v37

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "halbmarathon-cache-v36";
+const CACHE_NAME = "halbmarathon-cache-v37";
 
 const urlsToCache = [
   "./",
@@ -21,6 +21,7 @@ self.addEventListener("fetch", event => {
       .then(response => response || fetch(event.request))
   );
 });
+
 
 
 
