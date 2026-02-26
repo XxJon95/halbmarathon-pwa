a7c92c4fe99d84280347906b9d82b66b24229a3d
XxJon95 <jonas.riedinger@t-online.de>
2026-02-26 23:06:54 +0100

    add Firebase bootstrap and Google auth module setup

---
+++ auth.js
@@ -0,0 +1,21 @@
+﻿import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+const provider = new GoogleAuthProvider();
+
+export async function signInWithGoogle() {
+  return signInWithPopup(auth, provider);
+}
+
+export async function signOutUser() {
+  return signOut(auth);
+}
+
+export function onAuthChange(callback) {
+  return onAuthStateChanged(auth, callback);
+}
---
+++ firebase.js
@@ -0,0 +1,25 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  enableIndexedDbPersistence
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSVSQFvVZq55TciLzNoHifJUp754jIO4k",
+  authDomain: "halbmarathon-pwa.firebaseapp.com",
+  projectId: "halbmarathon-pwa",
+  storageBucket: "halbmarathon-pwa.firebasestorage.app",
+  messagingSenderId: "416871895562",
+  appId: "1:416871895562:web:0e7d8bd0b5eeb686c885f3"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+enableIndexedDbPersistence(db).catch(() => {
+  // Silent fallback if persistence is not available in this runtime.
+});
+
+export { app, auth, db, firebaseConfig };
