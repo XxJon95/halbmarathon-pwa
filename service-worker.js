f1e16c3c84c83244c8a0d097b7ee24d6a9ebbfd6
XxJon95 <jonas.riedinger@t-online.de>
2026-02-26 17:00:50 +0100

    Bump cache version to v50

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "halbmarathon-cache-v44";
+const CACHE_NAME = "halbmarathon-cache-v50";
 
 const urlsToCache = [
   "./",
@@ -15,44 +15,22 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
   );
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.match(event.request).then((response) => response || fetch(event.request))
+    )
+  );
+});
